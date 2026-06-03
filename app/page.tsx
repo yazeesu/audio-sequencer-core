a49fa771281b5e286938f69d5d204dd4150b4284
Eren Oğuz <threnkost48@gmail.com>
2026-06-03 07:29:12 +0300

    DEPS: Installing AuthJS for Session management.

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,7 @@
-"use client";
-
-import { InstrumentAudioEngine } from "@/src/audio/core";
-import { BreadBreadOverdrivenGuitarInstrument } from "@/src/audio/instruments/bread-bread-overdriven-guitar";
-import { TinpotsPianoInstrument } from "@/src/audio/instruments/tinpots-piano";
-import SimplePiano from "@/src/instruments/simple-piano/simple-piano";
-import { createPianoLayout } from "@/src/piano/layout";
-import { useEffect, useRef } from "react";
-
-const PIANO_LAYOUT = createPianoLayout(1, 4);
-
 export default function Home() {
-  const instrumentAudioEngineRef = useRef<InstrumentAudioEngine | null>(null);
-
-  useEffect(() => {
-    instrumentAudioEngineRef.current =
-      new BreadBreadOverdrivenGuitarInstrument();
-
-    return () => {
-      instrumentAudioEngineRef.current?.dispose();
-    };
-  }, []);
-
   return (
-    <div className="relative w-screen min-h-screen">
-      <div className="absolute bottom-8 w-full px-8">
-        <SimplePiano
-          audioEngine={instrumentAudioEngineRef}
-          layout={PIANO_LAYOUT}
-        >
-          <SimplePiano.SimplePianoKeyboard
-            renderWhiteKey={(note, style) => (
-              <SimplePiano.StandardWhiteKey note={note} style={style} />
-            )}
-            renderBlackKey={(note, style) => (
-              <SimplePiano.StandardBlackKey note={note} style={style} />
-            )}
-          />
-        </SimplePiano>
-      </div>
+    <div className="w-screen min-h-screen flex items-center justify-center">
+      <h1>Welcome to PolySonus</h1>
     </div>
   );
 }
